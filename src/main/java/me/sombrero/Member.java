e8bc8cbce7b7108fece32fa053aa6a3ff3450b7b
sombrero104 <sombrero.khy@gmail.com>
2020-10-09 10:53:03 +0900

    .

--- src/main/java/me/sombrero/Member.java
+++ src/main/java/me/sombrero/Member.java
@@ -9,6 +9,7 @@ import lombok.Setter;
  * 롬복 동작 원리:
  *      컴파일 시점에 애노테이션 프로세서를 사용하여
  *      소스코드의 AST(abstract syntax tree)를 조작한다.
+ *      컴파일할 때 특정한 애노테이션이 붙어있는 소스코드를 참조해서 또 다른 소스코드를 만든다.
  */
 @Getter @Setter
 @EqualsAndHashCode
